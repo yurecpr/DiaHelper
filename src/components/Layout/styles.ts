be084d0fc16b709b44fa7f0463de5a9f70e7c0e1
Yurii Bolharyn <yurecpr@gmail.com>
2024-06-02 10:45:20 +0200

    edit Header and Footer

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -8,15 +8,25 @@ export const LayoutComponent = styled.div`
   flex: 1;
 `;
 
+// export const Header = styled.header`
+//   display: flex;
+//   justify-content: space-between;
+//   flex-wrap: wrap;
+//   align-items: center;
+//   padding:12px 20px;
+//   width: 100%;
+//   background-color: rgba(0, 90, 117, 0.2);
+  
+// `;
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
   align-items: center;
-  padding:12px 20px;
+  padding: 12px 20px;
   width: 100%;
-  background-color: rgba(0, 90, 117, 0.8);
-  
+  background-color: rgba(0, 90, 117, 0.2);
+  z-index: 3; 
 `;
 
 export const HeaderLogoContainer = styled(Link)`
@@ -42,17 +52,40 @@ export const StyledNavLink = styled(NavLink)`
   color: white;
 `;
 
+// export const Main = styled.main`
+//   display: flex;
+//   flex-direction: column;
+//   flex: 1;
+//   position: relative;
+//   &::before {
+//     content: '';
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     z-index: 1;
+//     width: 100%;
+//     height: 100%;
+//     background-image: url(${Banner});
+//     background-repeat: no-repeat;
+//     background-size: cover;
+//     opacity: 0.3;
+//    }
+// `;
+
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
   flex: 1;
   position: relative;
+  margin-top: -100px;
+  margin-bottom :-100px ;
+  z-index: 1; 
   &::before {
     content: '';
     position: absolute;
     top: 0;
     left: 0;
-    z-index: 1;
+    z-index: 1; 
     width: 100%;
     height: 100%;
     background-image: url(${Banner});
@@ -67,9 +100,12 @@ export const Footer = styled.footer`
   align-items: center;
   padding: 12px;
   width: 100%;
-  background-color: rgba(0, 90, 117, 0.8);
+  background-color: rgba(0, 90, 117, 0.3);
   color: white;
+  position: relative; 
+  z-index:1;
 `;
+
 
 export const SocialIcons = styled.footer`
   display: flex;

cad3cda58d36960d3655fdf5fb1b042373de198b
Yurii Bolharyn <yurecpr@gmail.com>
2024-06-18 19:27:54 +0200

    edit AboutUs

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -13,204 +13,91 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedinIn, faXing } from "@fortawesome/free-brands-svg-icons";
 
+const developers = [
+  {
+    name: "Nataliia Kyrylenko",
+    role: "Frontend Developer",
+    linkedin: "https://www.linkedin.com/in/nata-kyrylenko/",
+    xing: "https://www.xing.com/profile/Nataliia_Kyrylenko/web_profiles?expandNeffi=true",
+    github: "https://github.com/NataKyrylenko",
+    img: "/src/assets/Team/Nata.jpg", 
+  },
+  {
+    name: "Yurii Bolharyn",
+    role: "Team Lead",
+    linkedin: "https://www.linkedin.com/in/yurii-bolharyn-30a92a293/",
+    xing: "https://www.xing.com/profile/Yurii_Bolharyn/web_profiles?expandNeffi=true",
+    github: "https://github.com/yurecpr",
+    img: "/src/assets/Team/Yurii.jpg",
+  },
+  {
+    name: "Svitlana Petrakova",
+    role: "Backend Developer",
+    linkedin: "https://www.linkedin.com/in/svitlana-petrakova-b67558314/",
+    xing: "#",
+    github: "#",
+    img: "/src/assets/Team/Sveta.jpg", 
+  },
+  {
+    name: "Yadviga Demianova",
+    role: "Backend Developer",
+    linkedin: "#/",
+    xing: "#",
+    github: "https://github.com/YadyaDeman",
+    img: "/src/assets/Team/", 
+  },
+  {
+    name: "Anastasiia Popova",
+    role: "QA",
+    linkedin: "http://www.linkedin.com/in/anastasiia-popova-851448314",
+    xing: "#https://www.xing.com/profile/Anastasiia_Popova048461",
+    github: "https://github.com/AnastasiiaPopova17",
+    img: "/src/assets/Team/Nastiia.jpg", 
+  },
+  {
+    name: "Diana Lukovsky",
+    role: "QA",
+    linkedin: "https://www.linkedin.com/in/diana-lukovsky/",
+    xing: "#",
+    github: "https://github.com/DianaLukovskyi",
+    img: "/src/assets/Team/Diana.jpg", 
+  },
+  {
+    name: "Alexei Ruscan",
+    role: "Frontend Developer",
+    linkedin: "#/",
+    xing: "#",
+    github: "https://github.com/AleksInPlay?tab=repositories",
+    img: "/src/assets/Team/Alexey.jpg", 
+  },
+  ];
 function AboutUsPage() {
   return (
     <AboutUsContainer>
       <AboutUsTitle>Our team:</AboutUsTitle>
       <AboutUsWrapper>
-        <DeveloperCard>
+        {developers.map((dev, index) => (
+          <DeveloperCard key={index}>
+            <DeveloperImg src={dev.img} alt={`${dev.name}'s photo`} />
             <DeveloperInfoContainer>
-                <DeveloperImg />
-                <DeveloperInfo>
-                    <DeveloperName>Nataliia Kyrylenko</DeveloperName>
-                    <DeveloperName>Frontend Developer</DeveloperName>
-                </DeveloperInfo>
+              <DeveloperInfo>
+                <DeveloperName>{dev.name}</DeveloperName>
+                <DeveloperName>{dev.role}</DeveloperName>
+              </DeveloperInfo>
             </DeveloperInfoContainer>
-          <IconContainer>
-            <ContactLink
-              href="https://www.linkedin.com/in/nata-kyrylenko/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
-            </ContactLink>
-            <ContactLink
-              href="https://www.xing.com/profile/Nataliia_Kyrylenko/web_profiles?expandNeffi=true"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faXing}  size="2x" />
-            </ContactLink>
-            
-            <ContactLink
-              href="https://github.com/NataKyrylenko"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             <FontAwesomeIcon icon={faGithub} size="2x"/>
-            </ContactLink>
-          </IconContainer>
-        </DeveloperCard>
-
-        <DeveloperCard>
-            <DeveloperInfoContainer>
-                <DeveloperImg />
-                <DeveloperInfo>
-                    <DeveloperName>Name</DeveloperName>
-                    <DeveloperName>Developer</DeveloperName>
-                </DeveloperInfo>
-            </DeveloperInfoContainer>
-          <IconContainer>
-            <ContactLink
-              href="#/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
-            </ContactLink>
-            <ContactLink
-              href="#"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faXing}  size="2x" />
-            </ContactLink>
-            
-            <ContactLink
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             <FontAwesomeIcon icon={faGithub} size="2x"/>
-            </ContactLink>
-          </IconContainer>
-        </DeveloperCard>
-
-        <DeveloperCard>
-            <DeveloperInfoContainer>
-                <DeveloperImg />
-                <DeveloperInfo>
-                    <DeveloperName>Name</DeveloperName>
-                    <DeveloperName>Developer</DeveloperName>
-                </DeveloperInfo>
-            </DeveloperInfoContainer>
-          <IconContainer>
-            <ContactLink
-              href="#/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
-            </ContactLink>
-            <ContactLink
-              href="#"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faXing}  size="2x" />
-            </ContactLink>
-            
-            <ContactLink
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             <FontAwesomeIcon icon={faGithub} size="2x"/>
-            </ContactLink>
-          </IconContainer>
-        </DeveloperCard>
-
-        <DeveloperCard>
-            <DeveloperInfoContainer>
-                <DeveloperImg />
-                <DeveloperInfo>
-                    <DeveloperName>Name</DeveloperName>
-                    <DeveloperName>Developer</DeveloperName>
-                </DeveloperInfo>
-            </DeveloperInfoContainer>
-          <IconContainer>
-            <ContactLink
-              href="#/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
-            </ContactLink>
-            <ContactLink
-              href="#"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faXing}  size="2x" />
-            </ContactLink>
-            
-            <ContactLink
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             <FontAwesomeIcon icon={faGithub} size="2x"/>
-            </ContactLink>
-          </IconContainer>
-        </DeveloperCard>
-
-        <DeveloperCard>
-            <DeveloperInfoContainer>
-                <DeveloperImg />
-                <DeveloperInfo>
-                    <DeveloperName>Name</DeveloperName>
-                    <DeveloperName>Developer</DeveloperName>
-                </DeveloperInfo>
-            </DeveloperInfoContainer>
-          <IconContainer>
-            <ContactLink
-              href="#/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
-            </ContactLink>
-            <ContactLink
-              href="#"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faXing}  size="2x" />
-            </ContactLink>
-            
-            <ContactLink
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             <FontAwesomeIcon icon={faGithub} size="2x"/>
-            </ContactLink>
-          </IconContainer>
-        </DeveloperCard>
-
-        <DeveloperCard>
-            <DeveloperInfoContainer>
-                <DeveloperImg />
-                <DeveloperInfo>
-                    <DeveloperName>Name</DeveloperName>
-                    <DeveloperName>Developer</DeveloperName>
-                </DeveloperInfo>
-            </DeveloperInfoContainer>
-          <IconContainer>
-            <ContactLink
-              href="#/"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faLinkedinIn} size="2x"/>
-            </ContactLink>
-            <ContactLink
-              href="#"
-              target="_blank"
-            >
-              <FontAwesomeIcon icon={faXing}  size="2x" />
-            </ContactLink>
-            
-            <ContactLink
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-             <FontAwesomeIcon icon={faGithub} size="2x"/>
-            </ContactLink>
-          </IconContainer>
-        </DeveloperCard>
-
-        
+            <IconContainer>
+              <ContactLink href={dev.linkedin} target="_blank">
+                <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+              </ContactLink>
+              <ContactLink href={dev.xing} target="_blank">
+                <FontAwesomeIcon icon={faXing} size="2x" />
+              </ContactLink>
+              <ContactLink href={dev.github} target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </ContactLink>
+            </IconContainer>
+          </DeveloperCard>
+        ))}
       </AboutUsWrapper>
     </AboutUsContainer>
   );
--- src/pages/AboutUsPage/styles.ts
+++ src/pages/AboutUsPage/styles.ts
@@ -9,38 +9,46 @@ export const AboutUsContainer = styled.div`
   z-index: 2;
 `;
 export const AboutUsTitle = styled.h2`
-font-size: 30px;
-color: white;
-
+  font-size: 30px;
+  color: white;
 `;
 export const AboutUsWrapper = styled.div`
-display: flex;
-flex-wrap: wrap;
-padding: 20px;
-align-items: center;
-gap: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 20px;
+  align-items: center;
+  gap: 20px;
 `;
 export const DeveloperCard = styled.div`
-display: flex;
-flex-direction: column;
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  margin: 10px;
+  /* border: 1px solid #ccc; */
+  border-radius: 5px;
+  width: auto;
+  text-align: center;
+  background-color:#394BBC;
 `;
 export const DeveloperInfoContainer = styled.div`
-display: flex;
-
-`
+  display: flex;
+`;
 export const DeveloperInfo = styled.div`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
-export const DeveloperName = styled.h3`
-`;
-
+export const DeveloperName = styled.h3``;
 
 export const DeveloperImg = styled.img`
+  width: 200px; 
+  height: 200px; 
+  border-radius: 50%; 
+  object-fit: cover; 
+  margin-bottom: 10px; 
 `;
 
 export const IconContainer = styled.div`
-display: flex;
-gap: 15px;
+  display: flex;
+  gap: 15px;
 `;
